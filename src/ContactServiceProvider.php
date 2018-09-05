0241939fc4e666ffefba68e1e2f5d077449899f9
appscyclone <duydt4.it@gmail.com>
2018-09-05 13:47:08 +0700

    Init Commit

---
+++ src/ContactServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Acc\Contact;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+    }
+
+    public function register()
+    {
+
+    }
+}
